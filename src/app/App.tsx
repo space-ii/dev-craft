d72d67d9dd020295078fa55f9c572f8e456391d0
P Z <pz@MacBook-Air-P.local>
2023-10-05 15:29:57 +0300

    navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./styles/index.scss";
 import { useTheme } from "./providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
 
 export default function App() {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
-      <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={"/"}>Главная</Link>
-      <Link to={"/about"}>О сайте</Link>
+      <Navbar />
       <AppRouter />
+      <button onClick={toggleTheme}>TOGGLE</button>
     </div>
   );
 }
