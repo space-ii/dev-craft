aa412e2d0df2357d23c15bee941b190a20a1b09e
P Z <pz@MacBook-Air-P.local>
2023-10-07 11:00:13 +0300

    hot-module-replacement

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,16 +3,44 @@ import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import { BuildOptions } from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+
+// export function buildPlugins({
+//   paths,
+//   isDev,
+// }: BuildOptions): webpack.WebpackPluginInstance[] {
+
+//   return [
+//     new HtmlWebpackPlugin({
+//       template: paths.html,
+//     }),
+//     // new webpack.AutomaticPrefetchPlugin(),
+//     new MiniCssExtractPlugin({
+//       filename: "css/[name].[contenthash:8].css",
+//       chunkFilename: "css/[name].[contenthash:8].css",
+//     }),
+//     new webpack.DefinePlugin({
+//       __IS_DEV__: JSON.stringify(isDev),
+//     }),
+//     new webpack.HotModuleReplacementPlugin(),
+//   ];
+// }
 
 export function buildPlugins({
   paths,
   isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+  const plugins = [];
+
+  if (isDev) {
+    plugins.push(new ReactRefreshWebpackPlugin());
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+  }
+
+  plugins.push(
     new HtmlWebpackPlugin({
       template: paths.html,
     }),
-    // new webpack.AutomaticPrefetchPlugin(),
     new MiniCssExtractPlugin({
       filename: "css/[name].[contenthash:8].css",
       chunkFilename: "css/[name].[contenthash:8].css",
@@ -20,5 +48,8 @@ export function buildPlugins({
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
-  ];
+    new webpack.HotModuleReplacementPlugin()
+  );
+
+  return plugins;
 }
